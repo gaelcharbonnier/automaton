d5420f55fe2663428f8039bc114922dd6176941d
Gaël Charbonnier <gcharbonnier@passman.fr>
2025-01-27 13:47:50 +0100

    Missing fields

--- src/main/java/com/gch/automaton/cache/LocalItem.java
+++ src/main/java/com/gch/automaton/cache/LocalItem.java
@@ -1,34 +1,70 @@
 package com.gch.automaton.cache;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public class LocalItem {
     private UUID uuid;
+    private Long unitId;
     private ItemType itemType;
     private UnitZoneType unitZoneType;
+    private Instant valueDate;
     private Object value;
 
-    public LocalItem(UUID uuid, ItemType itemType, UnitZoneType unitZoneType, Object value) {
+    public LocalItem(UUID uuid, Long unitId, ItemType itemType, UnitZoneType unitZoneType, Instant valueDate, Object value) {
         this.uuid = uuid;
+        this.unitId = unitId;
         this.itemType = itemType;
         this.unitZoneType = unitZoneType;
+        this.valueDate = valueDate;
         this.value = value;
     }
 
-
     public UUID getUuid() {
         return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public Long getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(Long unitId) {
+        this.unitId = unitId;
     }
 
     public ItemType getItemType() {
         return itemType;
     }
 
+    public void setItemType(ItemType itemType) {
+        this.itemType = itemType;
+    }
+
     public UnitZoneType getUnitZoneType() {
         return unitZoneType;
     }
 
+    public void setUnitZoneType(UnitZoneType unitZoneType) {
+        this.unitZoneType = unitZoneType;
+    }
+
+    public Instant getValueDate() {
+        return valueDate;
+    }
+
+    public void setValueDate(Instant valueDate) {
+        this.valueDate = valueDate;
+    }
+
     public Object getValue() {
         return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
     }
 }
